ce7a289a1ec38262fa29ce9e163e7a41e0949fe9
Ilya Lokalin <ilya-1974@yandex.ru>
2020-03-17 16:08:37 +0500

    feat: create convertation expr to Reverse Polish Notation

--- src/index.js
+++ src/index.js
@@ -4,112 +4,60 @@ function eval() {
 }
 
 function expressionCalculator(expr) {
-    const arrExpr = Array.from(expr.matchAll(/[\d]+|[+-]|[(]|[)]|[\*]|[\/]/g));
-    let isOperationStart = true;
-    let operationMinus = false;
-    const stack = [];
+    const arrExpr = expr.match(/[\d]+|[+-]|[(]|[)]|[\*]|[\/]/g);
+    const stackOperation = [],
+          outputList = [];
+    console.log(arrExpr)
+    arrExpr.forEach(item => {
 
-    const calculateStack = (operator, number) => {
-
-        if (stack.length > 0) {
-            let baseNumber = stack.pop();
-
-            if (/[\d]+/.test(baseNumber)) {
-                baseNumber = Number(baseNumber);
-
-                switch (operator) {
-                    case "+":
-                        baseNumber += Number(number);
-                        break;
-                    case "-":
-                        baseNumber -= Number(number);
-                        break;
-                    case "*":
-                        baseNumber *= Number(number);
-                        break;
-                    case "/":
-                        let divider = Number(number);
-
-                        if (divider > 0){
-                            baseNumber /= Number(number);
-                        } else {
-                            throw 'TypeError: Division by zero.';
-                        }
-                        
-                        break;
-
-                    default:
-                        console.log('Invalid operation', operator);
-                        break;
-                }
-
-                return baseNumber;
-            }
-
-            if (/[(]/.test(baseNumber)) {
-                return 0 - Number(number);
-            }
-
-            if (/[+-/*//]/.test(baseNumber) && operator === '-') {
-                return calculateStack(baseNumber, 0 - Number(number));
-            }
-
-            console.log("Ожидали число или операцию, а достали", baseNumber);
-
-        } else if (operator === '-') {
-            return 0 - Number(number);
-        } else {
-            console.log("Стек достиг 0 при ожидании вычисления", operator);
-        }
-    }
-
-    arrExpr.forEach(itemArr => {
-        item = itemArr[0];
-        console.log('На входе',item);
         if (/[\d]+/.test(item)) {
-            if (stack.length > 0) {
-                let operation = stack.pop();
-                console.log(operation, 'when item', item);
+            outputList.push(item);
+        }
 
-                if (/[+-/*//]/.test(operation)) {
-                    console.log('this is operation: stack ', operation, item);
-                    stack.push(calculateStack(operation, item));
-                }
-
-                if (/[(]/.test(operation)) {
-                    stack.push(operation);
-                    stack.push(item);
-                }
-
-                if (/[\d]+/.test(operation)) {
-                    console.log('Подряд две цифры ',operation, item);
-                }
+        if (/[+-]/.test(item)) {
+            const prevOperation = stackOperation.pop();
+            if (prevOperation === '*' || prevOperation === '/') {
+                outputList.push(prevOperation);
             } else {
-                stack.push(item);
-                console.log(item);
+                stackOperation.push(prevOperation);
+            }
+            stackOperation.push(item);
+        }
+
+        if (/[/*//]/.test(item)) {
+            const prevOperation = stackOperation.pop();
+            if (prevOperation === '*' || prevOperation === '/') {
+                outputList.push(prevOperation);
+            } else {
+                stackOperation.push(prevOperation);
+            }
+            stackOperation.push(item);
+        }
+
+        if (item === '(') {
+            stackOperation.push(item);
+        }
+
+        if (item ===')') {
+            isLookForBreaket = true;
+            let prevOperation;
+            while (stackOperation.length > 0 && isLookForBreaket) {
+                prevOperation = stackOperation.pop();
+
+                if (prevOperation !== '(') {
+                    outputList.push(prevOperation);
+                } else {
+                    isLookForBreaket = false;
+                }
             }
         }
-
-        if (/[+-/*//]/.test(item)) {
-            stack.push(item)
-        }
-
-        if (/[(]/.test(item)) {
-            stack.push(item)
-        }
-
-        if (/[)]/.test(item)) {
-            
-        }
-
-
     })
 
-    if (stack.length === 1) {
-        return stack.pop();
+    while (stackOperation.length > 0) {
+        outputList.push(stackOperation.pop());
     }
 
-    console.log('Беда огорчение... стек не равен одному', stack);
+    console.log(outputList);
 }
 
 module.exports = {
