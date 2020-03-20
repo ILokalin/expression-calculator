4274039f09889339a63cd52a1933ce80d9694fe3
ILokalin <51790923+ILokalin@users.noreply.github.com>
2020-03-20 21:45:40 +0500

    feat: create full algorithm with polish notation

--- src/index.js
+++ src/index.js
@@ -7,57 +7,121 @@ function expressionCalculator(expr) {
     const arrExpr = expr.match(/[\d]+|[+-]|[(]|[)]|[\*]|[\/]/g);
     const stackOperation = [],
           outputList = [];
-    console.log(arrExpr)
-    arrExpr.forEach(item => {
+    
+    const setRatingForOperator = operator => {
+        const ratirator = {}
 
+        if (operator === '/' || operator === '*') {
+            ratirator.rating = 2;
+        }
+
+        if (operator === '+' || operator === '-') {
+            ratirator.rating = 1;
+        }
+
+        if (operator === '(') {
+            ratirator.rating = 0;
+        }
+
+        ratirator.operator = operator
+
+        return ratirator;
+    }
+
+    const checkPriorityOperation = operator => {
+        let isLookForOperator = true;
+
+        while (stackOperation.length > 0 && isLookForOperator) {
+            const prevOperator = stackOperation.pop();
+            
+            if (prevOperator.rating >= operator.rating) {
+                outputList.push(prevOperator);
+            } else {
+                stackOperation.push(prevOperator)
+                isLookForOperator = false;
+            }
+        }
+
+        stackOperation.push(operator);
+    }
+
+    arrExpr.forEach(item => {
         if (/[\d]+/.test(item)) {
             outputList.push(item);
         }
 
-        if (/[+-]/.test(item)) {
-            const prevOperation = stackOperation.pop();
-            if (prevOperation === '*' || prevOperation === '/') {
-                outputList.push(prevOperation);
-            } else {
-                stackOperation.push(prevOperation);
-            }
-            stackOperation.push(item);
-        }
-
-        if (/[/*//]/.test(item)) {
-            const prevOperation = stackOperation.pop();
-            if (prevOperation === '*' || prevOperation === '/') {
-                outputList.push(prevOperation);
-            } else {
-                stackOperation.push(prevOperation);
-            }
-            stackOperation.push(item);
+        if (/[+-/*//]/.test(item)) {
+            checkPriorityOperation(setRatingForOperator(item));
         }
 
         if (item === '(') {
-            stackOperation.push(item);
+            stackOperation.push(setRatingForOperator(item));
         }
 
         if (item ===')') {
-            isLookForBreaket = true;
+            let isLookForBracket = true;
             let prevOperation;
-            while (stackOperation.length > 0 && isLookForBreaket) {
+
+            while (stackOperation.length > 0 && isLookForBracket) {
                 prevOperation = stackOperation.pop();
 
-                if (prevOperation !== '(') {
-                    outputList.push(prevOperation);
+                if (prevOperation.operator === '(') {
+                    isLookForBracket = false;
                 } else {
-                    isLookForBreaket = false;
+                    if (/[-+/*//]/.test(prevOperation.operator)) {
+                        outputList.push(prevOperation);
+                    }
+                    if (stackOperation.length === 0) {
+                        throw 'ExpressionError: Brackets must be paired';
+                    }
                 }
             }
         }
     })
 
+
+    if (stackOperation.findIndex(item => item.operator === '(') > -1) {
+        throw 'ExpressionError: Brackets must be paired';
+    }
+
     while (stackOperation.length > 0) {
         outputList.push(stackOperation.pop());
     }
 
-    console.log(outputList);
+    outputList.forEach(item => {
+        if (/[\d]+/.test(item)) {
+            stackOperation.push(item);
+        }
+
+        if (/[+-/*//]/.test(item.operator)) {
+            let regB = Number(stackOperation.pop());
+            let regA = Number(stackOperation.pop());
+
+            switch (item.operator) {
+                case '+':
+                    regA += regB;
+                    break;
+                case '-':
+                    regA -= regB;
+                    break;
+                case '/':
+                    if(regB === 0) {
+                        throw 'TypeError: Division by zero.';
+                    }
+                    regA /= regB;
+                    break;
+                case '*':
+                    regA *= regB;
+                    break;
+                default:
+                    throw 'Undefined operator';
+                    break;
+            }
+            stackOperation.push(regA);
+        }
+    })
+
+    return Math.round(stackOperation.pop() * 10000) / 10000;
 }
 
 module.exports = {
